a23bbc1e480e6e09f50cffa5e878c80d1e24c57c
v_puchunjie <v_puchunjie@baidu.com>
2018-11-06 12:32:31 +0800

    up demo

---
+++ app/src/main/java/com/baidu/wakaapp/avtivity/RecommendActivity.java
@@ -0,0 +1,1 @@
+package com.baidu.wakaapp.avtivity;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;import com.baidu.wakaapp.R;public class RecommendActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.fragment_recommend);    }}
